25f6adbd84181116e75bd65479f4c86bdc8543b4
Fredrik Storehaug <fredrik@varjo.com>
2022-04-22 10:09:45 +0200

    use resources to load asset

--- Runtime/Scripts/TrackingCharacterRigg.cs
+++ Runtime/Scripts/TrackingCharacterRigg.cs
@@ -7,14 +7,12 @@ namespace varjo.iksystem
     //this will implement the facade that the software interactis with to get a tracking avatar
     public class TrackingCharacterRigg
     {
-        //We need somethign to get this thing
-        //can use scriptable object, then we need one for each package when we want to load. 
-        private RigTrackingSetupMonoBehaviour _resourcePrefab;
+        private string _resourcePath = "PlayerCharacterIkRigg";
         private RigTrackingSetupMonoBehaviour _instance;
 
         public TrackingCharacterRigg(Transform rootparent, Transform headTarget, Transform leftArmTarget, Transform rightArmTarget)
         {
-            _instance = GameObject.Instantiate<RigTrackingSetupMonoBehaviour>(_resourcePrefab);
+            _instance = GameObject.Instantiate<RigTrackingSetupMonoBehaviour>(Resources.Load<RigTrackingSetupMonoBehaviour>(_resourcePath));
             _instance.Construct(rootparent, headTarget, leftArmTarget, rightArmTarget); //if we use zenject, we can turn this script into an installer and work from there.
 
         }
